13337447f59087724ca1a086072c2fcb00db2153
Zihan Zhao <zhaoz@royal-08.cs.wisc.edu>
2022-03-23 14:30:39 -0500

    Add multiple lib function declarations

--- user.h
+++ user.h
@@ -23,6 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int clone(void(*fcn)(void *, void *), void *arg1, void *arg2, void *stack);
+int join(void **stack);
 
 // ulib.c
 int stat(const char*, struct stat*);
@@ -37,3 +39,9 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+
+int thread_create(void(*)(void *, void *), void *, void *);
+int thread_join();
+void lock_acquire(lock_t *);
+void lock_release(lock_t *);
+void lock_init(lock_t *);
