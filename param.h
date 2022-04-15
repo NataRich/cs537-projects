8f1c1142774f8892176c6beb08554be2dbdaa2cd
Zihan Zhao <zhaoz@vm-instunix-12.cs.wisc.edu>
2022-04-14 19:55:03 -0500

    add clocksize 8

--- param.h
+++ param.h
@@ -1,6 +1,8 @@
 #ifndef PARAM_H
 #define PARAM_H
 
+#define CLOCKSIZE     8  // CLOCKSIZE represents N above
+
 #define NPROC        64  // maximum number of processes
 #define KSTACKSIZE 4096  // size of per-process kernel stack
 #define NCPU          8  // maximum number of CPUs
