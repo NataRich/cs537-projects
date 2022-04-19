d6cb5b5f6a7b6ebcff6b274a5d1459f63204cc3c
Zihan Zhao <zhaoz@royal-25.cs.wisc.edu>
2022-04-18 23:30:42 -0500

    comment out useless debug statement

--- kalloc.c
+++ kalloc.c
@@ -91,7 +91,7 @@ kalloc(void)
     kmem.freelist = r->next;
   if(kmem.use_lock)
     release(&kmem.lock);
-  cprintf("p4Debug : kalloc returns %d %x\n", PPN(V2P(r)), V2P(r));
+  //cprintf("p4Debug : kalloc returns %d %x\n", PPN(V2P(r)), V2P(r));
   return (char*)r;
 }
 
