572de0dff9a839a1ad591c96ae59d5b8c8becfe6
Zihan Zhao <zhaoz@rockhopper-08.cs.wisc.edu>
2022-05-04 12:05:28 -0500

    write files with original filenames

--- writer.c
+++ writer.c
@@ -10,9 +10,11 @@ void write_jpg(int fd, char *dir_path, f_jpg *jpg) {
     mkdir(dir_path, PERM);
 
   // write inum name
-  char path1[1024];
+  char path1[1024], path2[1024];
   snprintf(path1, 1024, "%s/file-%d.jpg", dir_path, jpg->inum);
-  int out_fd = open(path1, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+  snprintf(path2, 1024, "%s/%s", dir_path, jpg->filename);
+  int out_fd1 = open(path1, O_WRONLY | O_TRUNC | O_CREAT, 0666);
+  int out_fd2 = open(path2, O_WRONLY | O_TRUNC | O_CREAT, 0666);
   for (size_t i = 0; i < jpg->block_size; i++) {
     size_t write_size = block_size;
     if (i + 1 == jpg->block_size)
@@ -20,8 +22,10 @@ void write_jpg(int fd, char *dir_path, f_jpg *jpg) {
 
     char buffer[1024];
     seek_and_read_block(fd, BLOCK_OFFSET(jpg->data_blocks[i]), buffer);
-    write(out_fd, buffer, write_size);
+    write(out_fd1, buffer, write_size);
+    write(out_fd2, buffer, write_size);
   }
 
-  close(out_fd);
+  close(out_fd1);
+  close(out_fd2);
 }
